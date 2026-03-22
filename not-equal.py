3efe887a6e757f5345df0c57f33bcc832ed67ba7
CPUPetet <c.nwodo@abujaprep.com>
2026-03-22 17:32:29 +0100

    Not-Equal

---
+++ not-equal.py
@@ -0,0 +1,22 @@
+a = 10
+b = 14
+c = 14
+
+print(a != b)
+print(b != c)
+
+
+a = "Python"
+b = "C++"
+
+if a !=b:
+    print("These strings are not equal, so this string has been printed.")
+else:
+    print("These strings are equal, so this string has been printed.")
+
+
+a = 6
+b = 11
+
+if (a == b) != (b == 11):
+    print("Hello, world!")
