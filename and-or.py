62f2ce3e05ef472a91a8753b57fe4d7a1d3157b7
CPUPetet <c.nwodo@abujaprep.com>
2026-03-22 17:25:22 +0100

    And-Or

---
+++ and-or.py
@@ -0,0 +1,24 @@
+a = 8
+b = 29
+c  = 3
+
+if a and b and c:
+    print("All are true, so this string has been printed.")
+else:
+    print("At least one number is false, so this string has been printed.")
+
+
+
+a = 10
+b = -18
+c = 0    
+
+if a > 0 or b > 0:
+    print("At least one of these numbers is positive, so this string has been printed.")
+else:
+    print("All of these numbers are negative, so this string has been printed.")
+
+if b > 0 or c > 0:
+    print("Either of these numbers is positive, so this string has been printed.")
+else:
+    print("Both of these numbers are negative, so this string has been printed.")
